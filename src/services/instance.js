51b4eeaf086b168eb831306f410b496d7725fc58
Tharani-Kasiselvam <sherley.tharani@gmail.com>
2024-08-10 21:00:31 +0530

    backedn baseurl added

--- src/services/instance.js
+++ src/services/instance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // define the base url for the API
-const baseURL = 'http://localhost:5050';
+const baseURL = 'https://urlshortener-backend-wxz4.onrender.com';
 
     // create an axios instance
 const instance = axios.create({
